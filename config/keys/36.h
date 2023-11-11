70c364800e68bde53a62a6eedf5cb32f8644d97a
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-11-11 16:50:15 -0700

    feat(behaviors): try node-free bahavior and timeless home row mods

--- config/keys/36.h
+++ config/keys/36.h
@@ -6,6 +6,8 @@
   │ 20  21  22  23  24      │     25  26  27  28  29 │
   ╰───────────╮ 30  31  32  │ 33  34  35 ╭───────────╯
               ╰─────────────┴────────────╯
+*/
+/*
   ╭─────────────────────────┬─────────────────────────╮
   │ LT4 LT3 LT2 LT1 LT0     │     RT0 RT1 RT2 RT3 RT4 │
   │ LM4 LM3 LM2 LM1 LM0     │     RM0 RM1 RM2 RM3 RM4 │
@@ -59,3 +61,7 @@
 #define RH0 33  // right tHumb keys
 #define RH1 34
 #define RH2 35
+
+#define LEFT_KEYS LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4
+#define RIGHT_KEYS RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4
+#define THUMB_KEYS LH0 LH1 LH2 RH0 RH1 RH2
