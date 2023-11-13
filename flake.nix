61417b6a6205d87c8848d0c4570cb662038b168a
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-11-12 19:08:40 -0700

    feat(nix): add keymap image generator

--- flake.nix
+++ flake.nix
@@ -12,6 +12,16 @@
     boulder = {
       url = "github:BerkeleyTrue/nix-boulder-banner/2532c51d42e7bfba0bbe2a6d88f882dc85291cf4";
     };
+
+    keymap-drawer = {
+      url = "github:caksoylar/keymap-drawer";
+      flake = false;
+    };
+
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {flake-parts, ...}:
@@ -22,12 +32,42 @@
       ];
       perSystem = {
         pkgs,
-        inputs,
         config,
+        system,
         ...
-      }: {
+      }: let
+        inherit (inputs.poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) defaultPoetryOverrides mkPoetryApplication;
+
+        keymap-drawer = mkPoetryApplication {
+          projectDir = inputs.keymap-drawer;
+          overrides =
+            defaultPoetryOverrides.extend
+            (self: super: {
+              deptry =
+                super.deptry.overridePythonAttrs
+                (
+                  old: {
+                    buildInputs = (old.buildInputs or []) ++ [super.poetry];
+                  }
+                );
+            });
+        };
+
+        gen-keymap-img = pkgs.writeShellScriptBin "gen-keymap-img" ''
+          #!${pkgs.stdenv.shell}
+          ${keymap-drawer}/bin/keymap -c keymap-drawer/config.yml parse -z config/corne.keymap > keymap-drawer/corne.yaml
+          ${keymap-drawer}/bin/keymap -c keymap-drawer/config.yml draw keymap-drawer/corne.yaml > keymap-drawer/corne.svg
+        '';
+      in {
         formatter = pkgs.alejandra;
         boulder = {
+          commands = [
+            {
+              description = "Generate keymap image";
+              exec = gen-keymap-img;
+              category = "documentation";
+            }
+          ];
         };
         devShells.default = pkgs.mkShell {
           name = "zmk-keyboards";
@@ -36,6 +76,8 @@
           ];
           packages = with pkgs; [
             python311Packages.west
+            keymap-drawer
+            gen-keymap-img
           ];
         };
       };
